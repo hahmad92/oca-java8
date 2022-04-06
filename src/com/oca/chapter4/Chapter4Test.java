78fd9182798bb04a5bc1f8e07179bfccad452a50
Hammad Ahmad <hammadawan50@gmail.com>
2022-04-06 22:30:11 +0500

    Protected access modifier examples added.

--- src/com/oca/chapter4/Chapter4Test.java
+++ src/com/oca/chapter4/Chapter4Test.java
@@ -3,7 +3,7 @@ package com.oca.chapter4;
 public class Chapter4Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("asdasd");
 
 	}
 
--- src/com/oca/chapter4/pond/swan/Swan.java
+++ src/com/oca/chapter4/pond/swan/Swan.java
@@ -1,5 +1,6 @@
 package com.oca.chapter4.pond.swan;
 
+import com.oca.chapter4.pond.goose.Gosling;
 import com.oca.chapter4.pond.shore.Bird;
 
 public class Swan extends Bird { // but subclass of bird
@@ -18,5 +19,11 @@ public class Swan extends Bird { // but subclass of bird
 		Bird other = new Bird();
 		other.floatInWater(); // DOES NOT COMPILE
 		System.out.println(other.text); // DOES NOT COMPILE
+	}
+
+	public void printGosling(){
+		Gosling gosling = new Gosling();
+		gosling.floatInWater(); // DOES NOT COMPILE
+		System.out.println(gosling.text); // DOES NOT COMPILE
 	}
 }
